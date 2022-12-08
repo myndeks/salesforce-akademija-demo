f33b3a98d0700fca064575c9ae32408d6fe7d7f0
myndeks <myndeks@gmail.com>
2022-12-08 21:00:21 +0200

    first commit

--- scripts/bash/6.deploy-to-prod.sh
+++ scripts/bash/6.deploy-to-prod.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 set -e
 
-sfdx force:source:deploy -p ./force-app/ -u sf-akademija-prod --testlevel=RunLocalTests
+sfdx force:source:deploy -p C:\Users\MindaugasDragūnas\salesforce-akademija-demo\force-app\main\default\objects\aaa__c\aaa__c.object-meta.xml -u sf-akademija-prod --testlevel=RunLocalTests
